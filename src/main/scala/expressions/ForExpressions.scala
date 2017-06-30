dd69e879c925b06b130061b947de9477a220e43e
Oleksiy Blavat <blavat.alex@gmail.com>
2017-06-30 18:02:58 -0400

    organized demos, included demo with filtering

--- src/main/scala/expressions/ForExpressions.scala
+++ src/main/scala/expressions/ForExpressions.scala
@@ -1,12 +1,31 @@
 package expressions
 import java.io.File
+
 /**
   * Created by oblavat on 6/30/17.
   */
 object ForExpressions extends App {
-  val directoryContents = (new File("/")).listFiles()
-  for(i <- 0 until directoryContents.length) { //until excludes the last element of the range (0..9 = 0...8)
-    println(directoryContents(i))
+
+  def forDemo:String = {
+    val output:StringBuilder = new StringBuilder
+    val directoryContents:Array[File] = new File("/").listFiles()
+    for(i <- 0 until directoryContents.length) { //until excludes the last element of the range (0..9 = 0...8)
+      output ++= (directoryContents(i) + "\n")
+    }
+    output.toString()
   }
 
+  def forDemoWithFiltering:String = {
+    val output:StringBuilder = new StringBuilder
+    val directoryContents:Array[File] = new File("/").listFiles()
+    for (fileOrDirectory <- directoryContents if fileOrDirectory.isFile) {
+      output ++= (fileOrDirectory + "\n")
+    }
+    output.toString()
+  }
+
+
+  //demo calls to reduce console noise
+  println(forDemo)
+  println(Console.GREEN + forDemoWithFiltering)
 }
