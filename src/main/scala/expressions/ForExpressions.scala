dde57e13970ff8142b742c5d5e6ad16bf55e6182
Oleksiy Blavat <blavat.alex@gmail.com>
2017-07-31 21:30:42 -0400

    for expressions

--- src/main/scala/expressions/ForExpressions.scala
+++ src/main/scala/expressions/ForExpressions.scala
@@ -1,6 +1,8 @@
 package expressions
 import java.io.File
 
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by oblavat on 6/30/17.
   */
@@ -26,21 +28,39 @@ object ForExpressions extends App {
 
   def forNestedLoops:String = {
     val output:StringBuilder = new StringBuilder
-    val directoryContents:Array[File] = new File("/").listFiles()
+    val directoryContents:Array[File] = new File("/Users/oblavat/Documents").listFiles()
     for (
       files <- directoryContents
       if files.isFile;
-      fileContents = scala.io.Source.fromFile(files).getLines()
+      if files.getName.endsWith("rtf");
+      fileContents <- scala.io.Source.fromFile(files).getLines().toList
     ) {
       output ++= files + "\n"
       output ++= fileContents + "\n"
     }
     output.toString()
-
   }
+
+  def forBlockExpression:List[String] = {
+    val res:ListBuffer[String] = new ListBuffer[String]
+    val directoryContents:Array[File] = new File("/").listFiles()
+    for {
+      file <- directoryContents
+      if file.getName.endsWith("e")
+      if file.isDirectory
+      numFilesInside = file.listFiles().toList.length
+    } yield {
+      res.append(file.getName)
+      res.append(numFilesInside.toString)
+    }
+    res.toList
+  }
+
 
 
   //demo calls to reduce console noise
   println(forDemo)
   println(Console.GREEN + forDemoWithFiltering)
+  println(Console.YELLOW + forNestedLoops)
+  println(Console.BLUE + forBlockExpression)
 }
